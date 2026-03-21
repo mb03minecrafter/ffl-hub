5ed79561f91a6b70277c91b65d360c455e7d062c
Matthew Thomas Bickham <mattbick2003@gmail.com>
2026-03-20 19:36:23 -0500

    lets try this again.

--- src/Checkout/Map/CheckoutMap.php
+++ src/Checkout/Map/CheckoutMap.php
@@ -172,7 +172,7 @@ class CheckoutMap
         <?php
         $picker_html = ob_get_clean();
 
-        return $block_content . $picker_html;
+        return $picker_html . $block_content;
     }
 
     /**
